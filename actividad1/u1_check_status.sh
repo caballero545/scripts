b8db79fbb73828d31bd4c85a9ccb0cafd5f4309e
caballero <tadeovillegas538@gmail.com>
2026-02-17 22:27:02 -0700

    commit5

--- actividad1/u1_check_status.sh
+++ actividad1/u1_check_status.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "----estatus se cambio el coso?----"
 echo "nombre del equipo: $(hostname)"
-echo "ip actual: $(ip addr show enp0s8 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)"d/ -f1 | head -n 1)"
+echo "ip actual: $(ip addr show enp0s8 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)"
 echo "espacio en disco: "
 df -h / | grep /
