feca9f0f7cdedc12c89bb9b0d6e031bc340d5f75
caballero <tadeovillegas538@gmail.com>
2026-02-19 10:27:16 -0700

    commit dhcp1

---
+++ actividad2/u1_dhcp.sh
@@ -0,0 +1,7 @@
+#!/bash/bin
+if ! dpkg -l | grep -q isc-dhcp-server; then
+    echo "Instalando isc-dhcp-server..."
+    sudo apt-get update && sudo apt-get install -y isc-dhcp-server
+else
+    echo "El servicio ya está instalado."
+fi
