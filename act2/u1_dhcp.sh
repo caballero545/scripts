a0f3edf976526e05051c4779a12d7494acfd481e
caballero <tadeovillegas538@gmail.com>
2026-02-19 17:58:25 -0700

    dhcp

--- act2/u1_dhcp.sh
+++ act2/u1_dhcp.sh
@@ -16,35 +16,37 @@ dwnld_updt_dhcp() {
             echo "Se ha mantenido la versión actual."
         fi
     fi
+    read -p "Presiona [Enter] para volver al menú..."
 }
+while true; do
+	clear
+	echo "------------------------------------------"
+	echo "      MENU DE ADMINISTRACION DHCP"
+	echo "------------------------------------------"
+	echo "1. Descargar/actualizar DHCP"
+	echo "2. Configurar DHCP"
+	echo "3. Monitorear"
+	echo "4. Salir"
+	echo "------------------------------------------"
 
-clear
-echo "------------------------------------------"
-echo "      MENU DE ADMINISTRACION DHCP"
-echo "------------------------------------------"
-echo "1. Descargar/actualizar DHCP"
-echo "2. Configurar DHCP"
-echo "3. Monitorear"
-echo "4. Salir"
-echo "------------------------------------------"
+	read -p "Seleccione una opción (1-4): " opcn
 
-read -p "Seleccione una opción (1-4): " opcn
-
-case $opcn in
-    1)
-        dwnld_updt_dhcp
-        ;;
-    2)
-        echo "Esta opción se implementará en el siguiente paso."
-        ;;
-    3)
-        echo "Módulo de monitoreo aún no disponible."
-        ;;
-    4)
-        echo "Saliendo del script..."
-        exit 0
-        ;;
-    *)
-        echo "Opción no válida. Intente de nuevo."
-        ;;
-esac
+	case $opcn in
+    	1)
+        	dwnld_updt_dhcp
+        	;;
+    	2)
+        	echo "Esta opción se implementará en el siguiente paso."
+        	;;
+    	3)
+        	echo "Módulo de monitoreo aún no disponible."
+        	;;
+    	4)
+        	echo "Saliendo del script..."
+        	exit 0
+        	;;
+    	*)
+        	echo "Opción no válida. Intente de nuevo."
+        	;;
+	esac
+done
