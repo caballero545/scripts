cb56399916525c54b66d3d55ecd420051615885e
caballero <tadeovillegas538@gmail.com>
2026-02-24 17:13:07 -0700

    ssh

--- act4/main.sh
+++ act4/main.sh
@@ -2,7 +2,6 @@
 # main.sh
 source ./ins.sh
 
-# Variables que el MAIN recordará
 IP_FIJA=""
 SEGMENTO=""
 OCT_SRV=""
@@ -19,17 +18,39 @@ while true; do
     case $op in
         1) instalar_servicios ;;
         
-        2) establecer_ip_fija_logic ;;
+        2) 
+           # Capturamos la salida de la función para llenar las variables
+           RESP=$(establecer_ip_fija_logic)
+           if [ "$RESP" != "ERROR" ]; then
+               IP_FIJA=$RESP
+               SEGMENTO=$(echo $IP_FIJA | cut -d'.' -f1-3)
+               OCT_SRV=$(echo $IP_FIJA | cut -d'.' -f4)
+           fi ;;
 
-        3) config_dhcp_logic ;;
+        3) 
+           # Pasamos las variables a la función
+           config_dhcp_logic "$IP_FIJA" "$SEGMENTO" "$OCT_SRV" ;;
 
-        4) add_dominio_logic ;;
+        4) 
+           [[ -z "$IP_FIJA" ]] && { echo "Fija la IP primero"; sleep 2; continue; }
+           read -p "Nombre dominio: " DOM
+           # Pasamos el nombre y la IP
+           add_dominio_logic "$DOM" "$IP_FIJA"
+           [[ $? -eq 0 ]] && echo "Éxito" || echo "Error"
+           sleep 2 ;;
 
-        5) del_dominio_logic ;;
+        5) 
+           read -p "Dominio a borrar: " DOM_DEL
+           if del_dominio_logic "$DOM_DEL"; then
+               echo "Borrado correctamente."
+           else
+               echo "Error: Ese dominio NO existe."
+           fi
+           sleep 2 ;;
 
         6) grep "zone" /etc/bind/named.conf.local | cut -d'"' -f2; read -p "..." ;;
         
-        7) check_status_logic ;;
+        7) check_status_logic "$IP_FIJA" ;;
 
         8) exit 0 ;;
     esac
