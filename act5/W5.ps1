355fa8db660ed20eb4975bc3efcfbd1bdee9be70
caballero <tadeovillegas538@gmail.com>
2026-03-10 10:30:49 -0700

    FTPW

--- act5/W5.ps1
+++ act5/W5.ps1
@@ -1,15 +1,23 @@
-Get-LocalUser | Where-Object {
-Test-Path "C:\FTP\$($_.Name)"
-} | ForEach-Object {
+$VHOME="C:\FTP\vhome"
+
+Write-Host "================================="
+Write-Host "     USUARIOS FTP REGISTRADOS"
+Write-Host "================================="
+
+Get-ChildItem $VHOME -Directory | ForEach-Object {
 
 $user=$_.Name
 
 if(Get-LocalGroupMember reprobados -ErrorAction SilentlyContinue | Where {$_.Name -match $user}){
 $grupo="reprobados"
-}elseif(Get-LocalGroupMember recursadores | Where {$_.Name -match $user}){
+}
+elseif(Get-LocalGroupMember recursadores -ErrorAction SilentlyContinue | Where {$_.Name -match $user}){
 $grupo="recursadores"
 }
+else{
+$grupo="sin grupo"
+}
 
-Write-Host "$user - $grupo"
+Write-Host "$user  -  $grupo"
 
 }
